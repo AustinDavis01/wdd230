0fc0ec7fe88fbaf101806815342a584d30117b26
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-23 12:38:52 -0700

    navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,12 @@
 // Gather Elements needed
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
-const exit = document.getElementById("exit");
 
-// toggle the menu open and close
-menu.addEventListener("click", function(){
-    nav.style.display = "block";
-    menu.style.display = "none";
-    exit.style.display = "block";
-});
-
-// toggle the the exit button
-exit.addEventListener("click", function(){
-    nav.style.display = "none";
-    menu.style.display = "block";
-    exit.style.display = "none";
+menu.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    nav.classList.toggle("show");
 })
+
 
 
 
