efb1252baa26a08b67467d475eb91bd4212df9b6
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-01-23 20:53:48 -0700

    the

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
 
 menu.addEventListener("click", () => {
+    alert("working");
     menu.classList.toggle("show");
     nav.classList.toggle("show");
 })
