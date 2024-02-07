c52a202c0b7e6d77a763bbb04ad8fa23b942ae5d
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-02-07 12:00:15 -0700

    file

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -2,6 +2,35 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
 const member = document.getElementById("member");
+const modeButton = document.getElementById("mode");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+const footer = document.querySelector("footer");
+const title = document.querySelectorAll("h2");
+
+
+//Dark mode
+modeButton.addEventListener("click", function() {
+    modeButton.addEventListener("click", () => {
+        if (modeButton.textContent.includes("🕶️")) {
+            main.style.background = "#000000";
+            header.style.background = "#000000";
+            footer.style.background = "#000000";
+            body.style.color = "#FFFFFF";            
+            modeButton.textContent = "🔆";
+        } else {
+            main.style.background = "#FFFFFF";
+            header.style.background = "#FFFFFF";
+            footer.style.background = "#FFFFFF";
+            section.style.borderRadius = "10px";
+            body.style.color = "#000000";
+
+            modeButton.textContent = "🕶️";
+        }
+    });
+    
+});
 
 // event listener for menu button
 menu.addEventListener("click", () => {
