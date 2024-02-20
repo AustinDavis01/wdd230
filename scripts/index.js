734b5a1fc5807b1fb4be513b795a99c1b6303a02
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-02-20 09:57:20 -0700

    sounds good

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,12 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
 const temple = document.getElementById("templo");
+const visits = document.getElementById("visits");
+const tempature = document.getElementById("tempature");
+const location = document.getElementById("location");
+
+
+
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("show");
@@ -9,6 +15,19 @@ menu.addEventListener("click", () => {
     temple.classList.toggle("down");
     
 })
+
+// make passenger counter on home page.
+let numVisits = Number( localStorage.getItem("numVisits")) || 0;
+
+window.localStorage.setItem("numVisits", numVisits);
+
+if (numVisits != 0) {
+	visits.textContent = numVisits;
+} else {
+	visits.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
 
 
 
