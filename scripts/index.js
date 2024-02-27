e052fb1463d24010cb3b75525e132aedd5117d33
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-02-27 15:15:09 -0700

    yes

--- scripts/index.js
+++ scripts/index.js
@@ -44,5 +44,16 @@ numVisits++;
 
 window.localStorage.setItem("numVisits", numVisits);
 
+// grab elements
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
 
 
