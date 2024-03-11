12ceeaf5610120938f8e2bbb5c76f6df6594bc0d
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-03-11 15:41:47 -0500

    gym

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,14 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=2182407fe8adf3fe7aed195903bf92aa";
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        currentTemp.innerHTML = `${data.main.temp} F`;
+        weatherIcon.innerHTML = `${data.weather."0".icon}`
+
+    })
