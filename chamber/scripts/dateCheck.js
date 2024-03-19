f28c207953ad90e1df376f7b44ee54803a251f89
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-03-19 12:16:01 -0500

    lots

--- chamber/scripts/dateCheck.js
+++ chamber/scripts/dateCheck.js
@@ -5,11 +5,14 @@ let currentDate = date.getDay();
 
 if (currentDate == 1 || currentDate == 2 || currentDate == 3)
 {
+
     meet.style.display = "block";
+
+
 }
 
 exit.addEventListener("click", () => {
     meet.style.display = "none";
-    })
+})
 
 
