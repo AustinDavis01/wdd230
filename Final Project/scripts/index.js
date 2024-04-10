0d8ca6635bb460a17a5fade609d113dbb6993dd0
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-04-09 23:05:12 -0500

    save

--- Final Project/scripts/index.js
+++ Final Project/scripts/index.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
 
-
+// Hamburger Menu
 menu.addEventListener("click", () => {
     menu.classList.toggle("show");
     nav.classList.toggle("show");    
