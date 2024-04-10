f160b188aaa12f4974b75f580dc4cb7fc0288c0b
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-04-09 23:49:55 -0500

    commit

--- Final-Project/scripts/rental.js
+++ Final-Project/scripts/rental.js
@@ -1,4 +1,5 @@
-const url = "https://austindavis01.github.io/wdd230/Final Project/data/rental.json"
+const url = "https://austindavis01.github.io/wdd230/Final-Project/data/rental.json";
+const article = document.getElementById("print");
 
 
 fetch(url)
@@ -10,13 +11,36 @@ fetch(url)
 			let section = `<section">
 				<h2 class="heading">${repo.name}</h2>
 				<div>
-					<img class="bussimage" src="images/${repo.image}">
-					<p>${repo.adresses}</p>
-					<p>${repo.phone}</p>
-					<p>${repo.website}</p>
-					<p>${repo.membership}</p>
+					<div>
+						<img class="bussimage" src="images/${repo.image}">
+					</div>
+					<div>
+						<div>
+							<h3>Max Persons</h3>
+							<p>${repo.max-persons}</p>
+						</div>
+						<div>
+							<h3>Half Day (3hrs)</h3>
+							<p>${repo.half-day-reservation}</p>
+						</div>
+						<div>
+							<h3>Full Day</h3>
+							<p>${repo.full-day-reservation}</p>
+						</div>
+
+						<h3>Walk ins</h3>
+
+						<div>
+							<h3>Half Day (3hrs)</h3>
+							<p>${repo.half-day-walkin}</p>
+						</div>
+						<div>
+							<h3>Full Day</h3>
+							<p>${repo.full-day-walkin}</p>
+						</div>
+					</div>
 				</div>
 			</section>`
-			display.innerHTML += section;
+			article.innerHTML += section;
 		  });
 	})
